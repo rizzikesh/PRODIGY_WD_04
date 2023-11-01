ec7a7406dc82c7c69c87289be35f3da268fdaf9d
Rishikesh Bhor <143775871+rizzikesh@users.noreply.github.com>
2023-11-01 15:47:52 +0530

    Add files via upload

---
+++ Portfolio/script.js
@@ -0,0 +1,22 @@
+var typed = new Typed('#element', {
+  strings: ['Want to code together ??', "Let's Connect....."],
+  typeSpeed: 50,
+});
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
